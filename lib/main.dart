e8a1d403f5978f9d8556f2ad2c0303e588181657
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-16 18:29:07 +0530

    Error Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:map_view/map_view.dart';
 
 void main() {
+  // MapView.setApiKey("AIzaSyCcCaE1aoVaXs2paCi5a-CLei66s_QjMYE");
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MapPage(),
@@ -16,7 +18,9 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("GoogleMapBuilt"),
+      ),
     );
   }
 }
